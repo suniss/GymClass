7c12c6cff282fce2a2295537685ba942c7c5ad2c
sunisakc <sunisa.kan@gmail.com>
2021-09-19 23:41:45 +0200

    DB context not work

--- GymClass/Data/ApplicationDbContext.cs
+++ GymClass/Data/ApplicationDbContext.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+//using GymClass.Models.ViewModels.Students;
+//using GymClass.Models.Entities;
 
 namespace GymClass.Data
 {
     public class GymClassDbContext : IdentityDbContext
     {
-        public DbSet<GymClassDbContext> GymClass { get; set; }
-        //public DbSet<GymClass.Models.Entities.Course>  { get; set; }
+        public DbSet<GymClassDbContext>ApplicationUser { get; set; }
+       // public DbSet<GymClass.Models.Entities.GymClass> GymClass { get; set; }
 
         public GymClassDbContext(DbContextOptions<GymClassDbContext> options)
             : base(options)
