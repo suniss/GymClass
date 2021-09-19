641f595df403b881d801d4e31ee40335ae2cb78f
sunisakc <sunisa.kan@gmail.com>
2021-09-19 22:06:51 +0200

    Add ApplicationUserClass

--- GymClass/Data/ApplicationDbContext.cs
+++ GymClass/Data/ApplicationDbContext.cs
@@ -6,6 +6,8 @@ namespace GymClass.Data
     public class GymClassDbContext : IdentityDbContext
     {
         public DbSet<GymClassDbContext> GymClass { get; set; }
+        //public DbSet<GymClass.Models.Entities.Course>  { get; set; }
+
         public GymClassDbContext(DbContextOptions<GymClassDbContext> options)
             : base(options)
         {
