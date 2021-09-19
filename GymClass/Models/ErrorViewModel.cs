418242050d15309e26d0eecda394f37c36d09c95
sunisakc <sunisa.kan@gmail.com>
2021-09-19 21:17:41 +0200

    Add project files.

---
+++ GymClass/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GymClass.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ GymClass/Models/GymClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymClass.Models
+{
+    public class GymClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime EndTime { get { return StartTime + Duration; } }
+        public String Description { get; set; }
+    }
+}
